90f63fb2e4eb7766e103aa42827e69f58e77beb3
VarshaLohiya09 <valohiy1@in.ibm.com>
2024-07-24 11:54:21 +0530

    Add files via upload

---
+++ Appium/Project/Activity1.java
@@ -0,0 +1,63 @@
+package Activities;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class Activity1 {
+    // Driver Declaration
+    AndroidDriver driver;
+    WebDriver wait;
+
+    // Set up method
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.miui.calculator");
+        options.setAppActivity(".cal.CalculatorActivity");
+        options.noReset();
+
+        // Server Address
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
+
+        // Driver Initialization
+        driver = new AndroidDriver(serverURL, options);
+        //wait = new WebDriverWait(driver.Duration.ofSeconds(10));
+    }
+
+    // Test method
+    @Test
+    public void multiplyTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("btn_5_s")).click();
+        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+        driver.findElement(AppiumBy.id("btn_8_s")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+        // Assertion
+        Assert.assertEquals(result, "= 40");
+    }
+
+
+    // Tear down method
+    @AfterClass
+    public void tearDown() {
+        // Close the app
+        driver.quit();
+    }
+}
